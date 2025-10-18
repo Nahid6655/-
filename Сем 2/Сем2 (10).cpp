0fa03238dfd100ddc0e3f1a7a1483cc2e84dd89e
Nahid6655 <Tarverdiyev.Nahid6655@mail.ru>
2025-10-18 15:06:36 +0300

    Create Сем2 (10).cpp

---
+++ Сем 2/Сем2 (10).cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "RU");
+
+    int x;
+    cout << " Веедите число " << endl;
+    cin >> x;
+
+    if (x > 0)
+        cout << " число положительное \n";
+    if (x == 0)
+        cout << " число равно 0 \n";
+
+    if (x < 0)
+        cout << " число отрицательное \n";
+
+
+    return 0;
+}
