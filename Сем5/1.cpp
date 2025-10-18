7bff80f02b43a0ca6f9531f36388e34d735a1b2c
Nahid6655 <Tarverdiyev.Nahid6655@mail.ru>
2025-10-18 18:37:49 +0300

    Create 1.cpp

---
+++ Сем5/1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+int main() {
+    // Создание вектора целых чисел из 10 элементов
+    std::vector<int> numbers(10);
+    
+    // Заполнение вектора числами от 1 до 10
+    for (int i = 0; i < 10; ++i) {
+        numbers[i] = i + 1;
+    }
+    
+    // Вывод вектора с помощью for_each и лямбда-функции
+    std::cout << "Вектор чисел: ";
+    std::for_each(numbers.begin(), numbers.end(), [](int num) {
+        std::cout << num << " ";
+    });
+    std::cout << std::endl;
+    
+    return 0;
+}
